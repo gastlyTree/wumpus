dedd27cae4fcac5ddcb76bcd7bf97a8ba6ece5d6
cst204 <sutter1488@saskpolytech.ca>
2016-11-22 14:43:12 -0600

    Started working on the room class. Added the player class as a child of mob. which needs to me added to the mob_objects branch

--- wumpus/wumpus/Cave.cs
+++ wumpus/wumpus/Cave.cs
@@ -10,11 +10,15 @@ namespace wumpus
     public class Cave
     {
         #region Attributes
+        //The amounts of different mobs that will be added to the caves.
         public const int numBats = 2;
         public const int numPits = 2;
         public const int numWumpus = 1;
 
+        public List<Mob> mobsInTheGame = new List<Mob>();
 
+        //Trying to make a graph of type room, but running into problems. should ask Rob
+        //public UGraphMatrix<Room> Caves = new UGraphMatrix<Room>(); 
 
         #endregion
 
--- wumpus/wumpus/Room.cs
+++ wumpus/wumpus/Room.cs
@@ -8,6 +8,29 @@ namespace wumpus
 {
     public class Room
     {
+        //A list containing any mob that is in the room, Incuding the player
+        List<Mob> roomSquaters = new List<Mob>();
+        public int roomNumber;
 
+        /// <summary>
+        /// Will add a mob to the room, and then determine if the game is over
+        /// because of that action.
+        /// </summary>
+        /// <param name="mobToAdd">The mob that will be added tot he room</param>
+        /// <returns></returns>
+        public bool addToRoom(Mob mobToAdd)
+        {
+            bool isGameOver = false;
+
+            roomSquaters.Add(mobToAdd);
+
+            if(roomSquaters.OfType<Wumpus>().Any() && roomSquaters.OfType<Player>().Any() ||
+                roomSquaters.OfType<BottomlessPit>().Any() && roomSquaters.OfType<Player>().Any())
+            {
+                isGameOver = true;
+            }
+
+            return isGameOver;
+        }
     }
 }
