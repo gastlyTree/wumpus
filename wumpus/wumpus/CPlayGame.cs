8a01723f2cc998f5739d72e4fdbf9ea052a534c8
cst204 <sutter1488@saskpolytech.ca>
2016-11-25 13:49:48 -0600

    started the cosole implementation of the game

---
+++ wumpus/wumpus/CPlayGame.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wumpus
+{
+    /// <summary>
+    /// This will be resposible for running the game functions for the
+    /// console implementation of this game
+    /// </summary>
+    public class CPlayGame
+    {
+    }
+}
